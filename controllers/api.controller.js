111cc3bd2c9cb49f811e7e82513954caa6052a43
hungvnhd <hungvnhd>
2022-12-19 11:10:12 +0700

    api

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -5,3 +5,10 @@ module.exports.getAll = (req, res) => {
     res.status(200).json(data[0]);
   });
 };
+
+module.exports.getById = (req, res) => {
+  let userId = req.params.id;
+  db.execute("SELECT * FROM tbl_users WHERE id= ?", [userId]).then((data) => {
+    res.status(200).json(data[0]);
+  });
+};
--- routers/api.routes.js
+++ routers/api.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const apiController = require("../controllers/api.controller");
 
 router.get("/v1/users", apiController.getAll);
+router.get("/v1/users/:id", apiController.getById);
 
 module.exports = router;
