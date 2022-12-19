e9e51c739e0797279e44f4ccfbd5043e7f57152e
hungvnhd <hungvnhd>
2022-12-19 10:38:03 +0700

    api

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -2,6 +2,6 @@ const db = require("../models/db");
 
 module.exports.getAll = (req, res) => {
   db.execute("SELECT * FROM tbl_users").then((data) => {
-    res.status(200).json(data[0][0]);
+    res.status(200).json(data[0]);
   });
 };
