e77c2a186f2a4c98168dd87ac4815616309498ae
NGUYENDUCTHINH00 <ducthinhnd2004@gmail.com>
2022-12-20 17:30:28 +0700

    friends

--- models/db.js
+++ models/db.js
@@ -1,25 +1,25 @@
-const mysql = require("mysql2");
-// liên kết đến database: lấy và lưu dữ liệu trên db
-let pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "rikkeisocial",
-  password: "12345",
-  multipleStatements: true,
-});
-
-module.exports = pool.promise();
-
-//THINH
-
 // const mysql = require("mysql2");
 // // liên kết đến database: lấy và lưu dữ liệu trên db
 // let pool = mysql.createPool({
 //   host: "localhost",
 //   user: "root",
-//   database: "linkedin_schemas",
-//   password: "12345678",
+//   database: "rikkeisocial",
+//   password: "12345",
 //   multipleStatements: true,
 // });
 
 // module.exports = pool.promise();
+
+//THINH
+
+const mysql = require("mysql2");
+// liên kết đến database: lấy và lưu dữ liệu trên db
+let pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "linkedin_schemas",
+  password: "12345678",
+  multipleStatements: true,
+});
+
+module.exports = pool.promise();
--- routers/user.routes.js
+++ routers/user.routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 
 router.post("/friends/request/:id", userController.addingFriend);
-router.post("/friends/request/accept/:id", userController.acceptFriend);
+router.put("/friends/request/accept/:id", userController.acceptFriend);
 // router.get("/api/v1/friends/", userController.getFriends);
 module.exports = router;
