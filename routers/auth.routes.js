bc7ef4cab81f076a9c42c26c24d9a5de4c56aeca
hungvnhd <hungvnhd>
2022-12-20 17:25:19 +0700

    first commit

--- routers/auth.routes.js
+++ routers/auth.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+
 const authController = require("../controllers/auth.controller");
 
 router.post("/register", authController.signUpNewUser);
