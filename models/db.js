5f1268ab530209959b88355ac96baadd0f61db25
hungvnhd <hungvnhd>
2022-12-20 17:07:44 +0700

    friends

--- models/db.js
+++ models/db.js
@@ -9,3 +9,17 @@ let pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
+
+//THINH
+
+// const mysql = require("mysql2");
+// // liên kết đến database: lấy và lưu dữ liệu trên db
+// let pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "linkedin_schemas",
+//   password: "12345678",
+//   multipleStatements: true,
+// });
+
+// module.exports = pool.promise();
